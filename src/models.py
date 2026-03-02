bda0d19c8e773bd425d7f7d0ffbf5fad07fdea7f
jhonedwin99 <jhonelejalde@gmail.com>
2026-03-02 16:29:40 +0000

    modeldatosstarwars

--- src/models.py
+++ src/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List, Optional
+
 
 db = SQLAlchemy()
 
@@ -17,3 +19,88 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Users(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    last_name: Mapped[str] = mapped_column( String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    suscription_day: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+
+    favorites: Mapped[List["Favorites"]] = relationship(back_populates="users")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+
+# Tabla Planets:
+
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    rotation_period: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    orbital_period: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    climate: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    population: Mapped[str] = mapped_column(nullable=False)
+
+    favorites: Mapped[List["Favorites"]] = relationship(back_populates="planets")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+
+# Tabla Characters:
+
+class Characters(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    type: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    race: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    height: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    gender: Mapped[str] = mapped_column(nullable=False)
+
+    favorites: Mapped[List["Favorites"]] = relationship(back_populates="characters")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+
+# Tabla Favorites:
+
+class Favorites(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    # FK hacia Users (hijo de Users)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"),nullable=False)
+
+    # FK hacia Planets (hijo de Planets)
+    planet_id: Mapped[Optional[int]] = mapped_column(ForeignKey("planets.id"),nullable=False)
+    chracters_id: Mapped[Optional[int]] = mapped_column(ForeignKey("characters.id"),nullable=False)
+
+    # RELACIONES
+    user: Mapped["Users"] = relationship(back_populates="favorites")
+
+    planet: Mapped[Optional["Planets"]] = relationship(back_populates="favorites")
+
+    characters: Mapped[Optional["Characters"]] = relationship(back_populates="favorites")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+        }    
